657bbb9db26f37c442e0e853bb342ba5835859e3
yohanderjose2002 <yohanderjose2002@gmail.com>
2025-10-16 20:21:32 +0000

    up

--- client/js/main.js
+++ client/js/main.js
@@ -142,5 +142,18 @@ async function approveTransaction(id, button) {
   }
 }
 
+const sidebar = document.getElementById("sidebar");
+const openBtn = document.getElementById("openSidebar");
+const closeBtn = document.getElementById("closeSidebar");
+
+openBtn.addEventListener("click", () => {
+  sidebar.style.left = "0";
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.style.left = "-260px";
+});
+
+
 // ---------- INICIO ----------
 loadTransactions();
