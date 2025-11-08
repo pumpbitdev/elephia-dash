fc1eb8d01d39d40e4f33ca96819f8b301944d4e1
yohanderjose2002 <yohanderjose2002@gmail.com>
2025-11-08 00:14:20 +0000

    up

--- client/js/main.js
+++ client/js/main.js
@@ -57,8 +57,8 @@ function renderTable() {
   
       // --- Click en la fila para cargar métodos de pago ---
       row.addEventListener("click", () => {
-        loadPaymentMethods(tx.user_telegram_id);
-        //window.location.href = `/user?telegramId=${tx.user_telegram_id}`;
+        //loadPaymentMethods(tx.user_telegram_id);
+        window.location.href = `/user?telegramId=${tx.user_telegram_id}`;
       });
     });
   
